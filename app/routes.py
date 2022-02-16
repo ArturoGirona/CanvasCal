2e1bc769016b883d7c6f72d12fb9c518e9b39279
ArturoGirona <arturogirona@outlook.com>
2022-02-16 17:32:33 -0500

    began setting up skeleton backend for web app

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Arturo'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html', title='Calendar')
+
+@app.route('/todolist')
+def tasklist():
+    return render_template('todolist.html', title='Todo List')
