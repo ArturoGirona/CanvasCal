33b8f5943f4ab1e094cf86f189a145cb1b2d0562
Arturo Girona <arturogirona@outlook.com>
2022-03-27 00:23:40 -0400

    got links on main page working

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ def index():
     user = {'username': 'Arturo'}
     return render_template('MainPage.html', title='Home', user=user)
 
+'''
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html', title='Calendar')
@@ -14,3 +15,16 @@ def calendar():
 @app.route('/todolist')
 def tasklist():
     return render_template('todolist.html', title='Todo List')
+'''
+
+@app.route('/groups')
+def groups():
+    return render_template('GroupsPage.html', title='Groups')
+
+@app.route('/settings')
+def settings():
+    return render_template('SettingsPage.html', title='Settings')
+
+@app.route('/signin')
+def signin():
+    return render_template('SettingsPage.html', title='Sign In')
