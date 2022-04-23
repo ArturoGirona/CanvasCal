7ba4a6f91df3e380ea85d6592999ba61fedc58ab
lab2000man <lab2000man@gmail.com>
2022-04-23 00:12:11 -0400

    Added Flask logic to create/add account to DB
    Added form to add an account to the database. Also updated routes to support said form. Edited CreateAccountPage.html to use Flask forms, will fix styling issues.

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
@@ -9,3 +10,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class createAccForm(FlaskForm):##Form for creating account to add to db
+    email=StringField('Email', validators=[DataRequired(), Email()])
+    password=StringField('Password', validators=[DataRequired()])
+    password2=StringField('Password2', validators=[DataRequired(), EqualTo('password')])
+    submit=SubmitField('Create')
+
+def validate_email(self, email):    #Extra email validation tool to ensure email isn't taken yet!
+    user = User.query.filter_by(email=email.data)
+    if user is not None:
+        raise ValidationError('This username is taken!')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, createAccForm
 from app.models import User
 
 @app.route('/')
@@ -57,9 +57,17 @@ def signin():
         return redirect(url_for('index'))
     return render_template('SignInPage.html', title='Sign In', form=form)
 
-@app.route('/createAcc')
-def createAcc():
-    return render_template('CreateAccountPage.html', title='CreateAcc')
+@app.route('/createAcc', methods=['POST', 'GET'])   ##Updated createAcc route to use createAcc Form and 
+def createAcc():                                    ##Accompanying logic
+    flash('Test')
+    form = createAccForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('signin'))
+    return render_template('CreateAccountPage.html', title='Create Account', form=form)
 
 
 @app.route('/signout')
